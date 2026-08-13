61e3692ae10361f38a7fad64bf5ac95b14092a5c
halimroslan <abdulhalimroslan@gmail.com>
2026-08-13 23:31:32 +0800

    Fix player state leakage on channel switch by forcing remount with key prop

--- src/App.tsx
+++ src/App.tsx
@@ -112,7 +112,7 @@ function App() {
                     <span className="badge-live-dot"></span> LIVE
                   </span>
                 </div>
-                <Player channel={activeChannel} />
+                <Player key={activeChannel.contentId} channel={activeChannel} />
               </section>
             )}
 
